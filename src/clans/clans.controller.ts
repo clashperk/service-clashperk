6784e7612f6b0b8b26c03dc045db616565caed2b
suvajit <suvajit.me@gmail.com>
2023-02-06 22:50:05 +0530

    1 min cache

--- src/clans/clans.controller.ts
+++ src/clans/clans.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Header, Param, UseGuards } from '@nestjs/common';
+import { Role, Roles } from '../auth/decorators/roles.decorators';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { ClansService } from './clans.service';
 
 @Controller('clans')
 export class ClansController {
     constructor(private clansService: ClansService) {}
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    @Header('Cache-Control', 'max-age=60')
     @Get('/capital-donations/:clanTag')
     async getCapitalDonations(@Param('clanTag') clanTag: string) {
         return this.clansService.getCapitalDonations(clanTag);
