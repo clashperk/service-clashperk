8c0723dd3c6c6b7a8745e20033f9899cf127c4e8
suvajit <suvajit.me@gmail.com>
2023-08-14 00:57:53 +0530

    morgan logger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import morgan from 'morgan';
 import { AppModule } from './app.module';
 
+declare module 'express' {
+    interface Request {
+        user?: any;
+    }
+}
+
 async function bootstrap() {
     const logger = new Logger('HTTP');
 
@@ -12,11 +18,33 @@ async function bootstrap() {
 
     app.enableCors();
     app.use(
-        morgan('dev', {
-            stream: {
-                write: (message: string) => logger.log(message.trim())
+        morgan(
+            function (tokens, req, res) {
+                const status = res.statusCode;
+                const color =
+                    status >= 500
+                        ? 31 // red
+                        : status >= 400
+                        ? 33 // yellow
+                        : status >= 300
+                        ? 36 // cyan
+                        : status >= 200
+                        ? 32 // green
+                        : 0; // no color
+                return [
+                    `\x1b[0m${tokens.method(req, res)}`,
+                    `${tokens.url(req, res)} \x1b[` + color + `m${tokens.status(req, res)}\x1b[0m`,
+                    `${tokens['response-time'](req, res)} ms - ${tokens['remote-addr'](req, res)}\x1b[0m`,
+                    // @ts-expect-error lol
+                    req.user?.username ?? ''
+                ].join(' ');
+            },
+            {
+                stream: {
+                    write: (message: string) => logger.log(message.trim())
+                }
             }
-        })
+        )
     );
 
     const port: number = configService.get<number>('PORT');
